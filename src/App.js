c28abb4a17e98cf3be7b1ed2280a0af6c515ab30
sk2597 <smruti.karade25@gmail.com>
2024-06-25 15:44:36 +0530

    added head component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Head from "./components/Head";
 
 function App() {
   return (
     <div className="App">
       <h1 className="bg-red-400">Hello World!</h1>
+      <Head />
     </div>
   );
 }
